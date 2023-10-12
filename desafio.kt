8105f49798be410f9e307af5526f88b254378e9e
Carolina Viana <carolina.sov@gmail.com>
2023-10-12 01:17:25 -0300

    Atualização do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+open class Usuario(var nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+class Aluno(nome: String): Usuario(nome=nome){
+    override fun toString() = nome
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Professor(nome: String): Usuario(nome=nome)
+
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60){
+    override fun toString() = "Aula: $nome - Duração: $duracao min."
+}
+
+data class Formacao(var nome: String, val nivel: Nivel, val conteudos: MutableList<ConteudoEducacional>, val professores: MutableList<Professor>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg alunos: Aluno) {
+        for (aluno in alunos) inscritos.add(aluno)
+    }
+    
+    fun listaDeMatriculados() {
+        println("Matriculados: ${inscritos.sortedBy {it.nome}}")
+    }
+    
+    fun ementa(){
+        println("Ementa: $conteudos")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+    val aula1 = ConteudoEducacional("Introdução Prática" , 120)
+    val aula2 = ConteudoEducacional("Estruturas de Controle de Fluxo")
+    val aula3 = ConteudoEducacional("Coleções")
+    val aula4 = ConteudoEducacional("Orientação a Objetos" , 120)
+    
+    val professores = mutableListOf(Professor("Elisa"), Professor("Renilda"))
+    val bianca = Aluno("Bianca")
+    
+    val formacaoKotlin = Formacao("Kotlin", Nivel.INTERMEDIARIO, mutableListOf(aula1, aula2, aula3, aula4), professores)
+    
+    formacaoKotlin.matricular(bianca, Aluno("Pedro"), Aluno("Fabiano"), Aluno("Carolina"))
+    
+    formacaoKotlin.ementa()
+    
+    formacaoKotlin.listaDeMatriculados() 
+    }
